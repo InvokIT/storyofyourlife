faf732965c6774faeaf75ca045386e3e4190ca2d
Sebastian Jørgensen <sj@invokit.dk>
2018-09-06 17:05:49 +0200

    more oauth2 wip

--- src/oauth2-client.js
+++ src/oauth2-client.js
@@ -1,6 +1,6 @@
 //@flow
 import queryString from "query-string";
-import { v4 as uuid } from "uuid";
+import uuid from "uuid/v4";
 import type { Storage } from "./storage";
 
 type Callback = () => void | Promise<void>;
@@ -8,21 +8,47 @@ type Callback = () => void | Promise<void>;
 type ProviderConfig = {
     name: string,
     authorization_uri: string,
-    params: {
-        client_id: string,
-        [string]: string
-    }
+    client_id: string,
+    redirect_uri: ?string,
+    scope: ?string
 };
 
 type ReturnUri = string | (provider: string) => string;
 
-type TokenStorage = Storage<{[provider: string]: string}>;
+type Authorization = {
+    provider: string,
+    token: string,
+    expiresAt: ?number
+}
+
+type AuthStorage = Storage<{[provider: string]: Authorization}>;
 
 type OAuth2Options = {
-    tokenStorage: TokenStorage,
+    authStorage: AuthStorage,
     stateStorage: Storage<string>,
     returnUri: ReturnUri
 };
+
+type Oauth2SuccessResponse = {
+    access_token: string,
+    expires_in: string,
+    scope: ?string,
+    state: string
+}
+
+type Oauth2ErrorResponse = {
+    error: 
+        "invalid_request"
+        | "unauthorized_client"
+        | "access_denied"
+        | "unsupported_response_type"
+        | "invalid_scope"
+        | "server_error"
+        | "temporarily_unavailable",
+    error_description: ?string,
+    error_uri: ?string,
+    state: string
+}
 
 const generateState = (providerName: string) => {
     return btoa(JSON.stringify({
@@ -32,7 +58,7 @@ const generateState = (providerName: string) => {
 };
 
 const parseState = (state: string) => {
-    const s = JSON.parse(atob(state));
+    const s = (JSON.parse(atob(state)): {p:string, id: string});
     return {
         provider: s.p,
         id: s.id
@@ -41,14 +67,14 @@ const parseState = (state: string) => {
 
 export default class Oauth2Client {
     _returnUri: ReturnUri;
-    _tokenStorage: TokenStorage;
+    _authStorage: AuthStorage;
     _stateStorage: Storage<string>;
     +_providers = new Map<string, ProviderConfig>();
     +_beforeAuthCallbacks: Callback[] = [];
 
     constructor(options: OAuth2Options) {
         this._returnUri = options.returnUri;
-        this._tokenStorage = options.tokenStorage;
+        this._authStorage = options.authStorage;
         this._stateStorage = options.stateStorage;
     }
 
@@ -58,10 +84,11 @@ export default class Oauth2Client {
         this._providers.set(providerName, providerConfig);
     }
 
-    getToken(provider: string) {
-        const tokens = this._tokenStorage.load();
-        if (tokens && tokens[provider]) {
-            return tokens[provider];
+    getAuthorization(provider: string) {
+        const authorizations = this._authStorage.load();
+        if (authorizations && authorizations[provider]) {
+            // TODO return null of token is expired
+            return authorizations[provider];
         } else {
             return null;
         }
@@ -80,18 +107,15 @@ export default class Oauth2Client {
 
         await Promise.all(this._beforeAuthCallbacks.map(cb => cb()));
 
-        let returnUri = this._returnUri;
-        if (typeof returnUri === "function") {
-            returnUri = returnUri(providerName);
-        }
-
         const state = generateState(providerName);
         this._stateStorage.save(state);
 
         const qs = queryString.stringify({
-            redirect_uri: returnUri,
-            state: state,
-            ...providerConfig.params
+            response_type: "token",
+            client_id: providerConfig.client_id,
+            redirect_uri: providerConfig.redirect_uri || null,
+            scope: providerConfig.scope || null,
+            state: state
         });
 
         const authUri = `${providerConfig.authorization_uri}?${qs}`;
@@ -99,12 +123,37 @@ export default class Oauth2Client {
         location.href = authUri;
     }
 
-    finishAuthorization(providerName: string) {
-        const params = queryString.parse(location.hash);
-        
+    finishAuthorization() {
+        const response: Oauth2SuccessResponse | Oauth2ErrorResponse = (queryString.parse(location.hash): any);
 
-        if (params.hasOwnProperty("error")) {
-            //TODO
+        if (!response.state) {
+            // todo log
+            return;
+        }
+
+        const savedState = this._stateStorage.load();
+
+        if (savedState !== response.state) {
+            throw new Error("state from response does not match saved state.")
+        }
+
+        const parsedState = parseState(response.state);
+        const provider = parsedState.provider;
+
+        if (response.access_token) {
+            const successResponse: Oauth2SuccessResponse = (response: any);
+            const accessToken = successResponse.access_token;
+            const expiresIn = successResponse.expires_in;
+
+            const auths = this._authStorage.load();
+            auths[provider] = {
+                provider,
+                token: accessToken,
+                expiresAt: expiresIn ? Date.now() + (parseInt(expiresIn, 10) * 1000) : null
+            };
+            this._authStorage.save(auths);
+        } else {
+            const errorResponse: Oauth2ErrorResponse = (response: any);
         }
     }
 }
